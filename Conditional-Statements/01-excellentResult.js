0ae6c9b2d099042104461ea4816718e30c4a9f7f
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-09-30 13:58:18 +0300

    01-excellentResult

---
+++ Conditional-Statements/01-excellentResult.js
@@ -0,0 +1,10 @@
+function excellentResult(input) {
+    let grade = Number(input[0]);
+
+    if (grade === 6) {
+        console.log('Excellent!');
+    } else if (grade === 5.50) {
+        console.log('Excellent!');
+    }
+}
+excellentResult(['6']);
