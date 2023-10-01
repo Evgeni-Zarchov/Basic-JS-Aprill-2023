ccbae394d4b1be3b75ee7a350eba845a0451d131
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-01 14:01:13 +0300

    Create 06-maxNumber.js

---
+++ While-Loop/06-maxNumber.js
@@ -0,0 +1,17 @@
+function minNumber(input) {
+    let number = Number.MIN_SAFE_INTEGER;
+    let index = 0;
+    let currentInput = input[index];
+    while (currentInput !== "Stop") {
+        let numberInput = Number(currentInput);
+        if (numberInput > number) {
+            number = numberInput;
+        }
+        index++;
+        currentInput = input[index];
+
+    }
+    console.log(number);
+
+}
+minNumber(["100", "99", "80", "70", "Stop"]);
