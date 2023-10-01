95aba1b76cb12e5213de1fc92b88a7ac52be5bda
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-01 13:30:07 +0300

    Create 05-charSequence.js

---
+++ For-Loops/05-charSequence.js
@@ -0,0 +1,8 @@
+function characterSequence(input) {
+    let word = input[0];
+    for (let i = 0; i < word.length; i++) {
+        let text = word[i];
+        console.log(text);
+    }
+}
+characterSequence(['softuni']);
